b94e4f8b99db9e1bc96e61caeb12e90f99aae1db
satya2221 <satyaghifari12@gmail.com>
2025-04-07 16:28:05 +0700

    feat: app authentication for login

---
+++ apps/authentications/views.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.views.generic import View
+
+# Create your views here.
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+
+        messages.error(request, "Invalid Credential")
+        return redirect('login')
+    
+def logout_view(request):
+    logout(request)
+
+    return redirect('login')
