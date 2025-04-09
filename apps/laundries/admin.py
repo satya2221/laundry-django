d7f6740286d6cc53f4ee53b23dd39fc333d21d09
satya2221 <satyaghifari12@gmail.com>
2025-04-09 10:49:33 +0700

    feat: Add laundry to admin panel

--- apps/laundries/admin.py
+++ apps/laundries/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import LaundryOrder, LaundryItem
 
 # Register your models here.
+@admin.register(LaundryOrder)
+class LaundryOrderAdmin(admin.ModelAdmin):
+    list_display = ('updated_at','customer', 'received_date', 'expected_completion_date', 'status')
+    list_filter = ('customer', 'status')
+    search_fields = ('customer', )
+
+@admin.register(LaundryItem)
+class LaundryItemAdmin(admin.ModelAdmin):
+    list_display = ('updated_at','order', 'quantity', 'price_per_quantity', )
+    search_fields = ('order', )
