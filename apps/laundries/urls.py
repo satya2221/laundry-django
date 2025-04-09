1842e5619b6e4febafe4767f819712a5d866e9f5
satya2221 <satyaghifari12@gmail.com>
2025-04-09 14:49:58 +0700

    feat: html for creare laundry order

--- apps/laundries/urls.py
+++ apps/laundries/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import LaundryOrderListView
+from .views import LaundryOrderListView, CreateLaundryOrder
 
 urlpatterns = [
     path("", LaundryOrderListView.as_view(), name="laundry-order"),
+    path("createOrder", CreateLaundryOrder.as_view(), name="create-order")
 ]
